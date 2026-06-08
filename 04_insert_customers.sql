c91aa3d54337ea151020d9fe16b95be275b7ef27
Fidan Umudova <fidanumudova05@gmail.com>
2026-06-08 13:04:46 +0400

    Create 04_insert_customers.sql

---
+++ 04_insert_customers.sql
@@ -0,0 +1,6 @@
+INSERT INTO customers VALUES
+(1,'Ali','Mammadov','ali@gmail.com','Baku'),
+(2,'Nigar','Aliyeva','nigar@gmail.com','Baku'),
+(3,'Rashad','Hasanov','rashad@yahoo.com','Ganja'),
+(4,'Aysel','Karimova','aysel@gmail.com','Sumqayit'),
+(5,'Murad','Safarov','murad@gmail.com','Baku');
